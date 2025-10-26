3fb4453bacf393efc67bc99babbdbd15e641ebcb
asn49k <stefanalexnuta@gmail.com>
2025-10-26 11:22:04 +0000

    first

---
+++ options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+-- Tabs & indentation
+vim.opt.tabstop = 4        -- Number of visual spaces per TAB
+vim.opt.shiftwidth = 4     -- Number of spaces to use for autoindent
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
